dfe4ae7b2f3e438907d9f0064d96fdb452c0d5c8
mattferraz <mtshs.ferraz@gmail.com>
2022-11-15 16:41:02 -0300

    commit pratica 03

--- Git-Aula-2022-2/src/HelloWorld.java
+++ Git-Aula-2022-2/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Commit Bash+Eclipse!");
+		System.out.println("Mateus: Pratica 03 - Git Básico");
 	}
 
 }
