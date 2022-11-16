4e9a98cb27576fa7928172a378f2951d14011266
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2022-11-16 11:09:11 -0300

    Commit Main 2

--- Git-Aula-2022-2/src/HelloWorld.java
+++ Git-Aula-2022-2/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
                 System.out.println("Commit Bash!");
-                System.out.println("Branch Main + Bash");
+                System.out.println("Branch Main");
 	}
 
 }
