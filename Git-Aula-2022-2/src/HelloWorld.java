c0b70fc8ee3e05383105560bd8472073401be231
PeterNV <114534711+PeterNV@users.noreply.github.com>
2022-11-22 13:33:34 -0300

    COMMIT REALIZADO POR PEDRO VILA NOVA

--- Git-Aula-2022-2/src/HelloWorld.java
+++ Git-Aula-2022-2/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Commit Bash+Eclipse!");
 		System.out.println("Branch Main + Bash + Eclipse");
 		System.out.println("Mateus: Pratica 03 - Git Básico");
+		System.out.println("Pedro Vila Nova (@PeterNV). COMMIT da Prática 03");
 	}
 
 }
